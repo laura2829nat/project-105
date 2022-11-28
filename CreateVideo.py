41735626ca407028f376d98d0a5d8d76c818f31e
laura2829nat <96764335+laura2829nat@users.noreply.github.com>
2022-11-28 20:37:15 +0800

    Add files via upload

--- CreateVideo.py
+++ CreateVideo.py
@@ -1,7 +1,7 @@
 import os
 import cv2
 
-path = "Images"
+path = "Images/"
 
 images = []
 
@@ -15,15 +15,20 @@ for file in os.listdir(path):
                
         images.append(file_name)
 
+print(len(images))
 count = len(images)
-frame = cv2.imread(images[0])
-height, width, channels = frame.shape
-size = (width, height)
-print(size)
 
-out = cv2.VideoWriter("project.avi", cv2.VideoWriter_fourcc(*"DIVX"), 0.8, size)
-for i in range(count-1,0, -1):
-    frame = cv2.imread(images[i])
-    out.write(frame)
-out.release()
+
+frame = cv2.imread(images[0])
+height, width, layers = frame.shape
+size = (width,height)
+
+out = cv2.VideoWriter('project.avi',cv2.VideoWriter_fourcc(*'DIVX'), 0.8, size)
+for i in range(0,count):
+     print(images[i])
+     frame = cv2.imread(images[i])
+     out.write(frame)
+    
+out.release() # releasing the video generated
 print("done")
+
